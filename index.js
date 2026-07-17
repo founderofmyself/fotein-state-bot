68d4040224b757c4555fd8043e8e12ac42180d03
founderofmyself <lolitsthefamousjake@hotmail.com>
2026-07-17 16:16:40 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+const { Client, GatewayIntentBits } = require("discord.js");
+require("dotenv").config();
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent
+    ]
+});
+
+client.once("ready", () => {
+    console.log(`${client.user.tag} is online!`);
+});
+
+client.login(process.env.TOKEN);
