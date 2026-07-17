075639b973bdb5f86d3d29f381f62fd879fe5066
founderofmyself <lolitsthefamousjake@hotmail.com>
2026-07-17 16:21:09 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-const { Client, GatewayIntentBits } = require("discord.js");
+const {
+    Client,
+    GatewayIntentBits,
+    Collection
+} = require("discord.js");
+
+const fs = require("fs");
 require("dotenv").config();
+
 
 const client = new Client({
     intents: [
@@ -10,8 +17,57 @@ const client = new Client({
     ]
 });
 
-client.once("ready", () => {
-    console.log(`${client.user.tag} is online!`);
+
+client.commands = new Collection();
+
+
+// Load Commands
+const commandFiles = fs.readdirSync("./commands")
+.filter(file => file.endsWith(".js"));
+
+
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+
+    client.commands.set(
+        command.data.name,
+        command
+    );
+}
+
+
+// Load Events
+const eventFiles = fs.readdirSync("./events")
+.filter(file => file.endsWith(".js"));
+
+
+for (const file of eventFiles) {
+    const event = require(`./events/${file}`);
+
+    client.once(
+        event.name,
+        (...args) => event.execute(...args)
+    );
+}
+
+
+// Slash command handler
+client.on("interactionCreate", async interaction => {
+
+    if (!interaction.isChatInputCommand()) return;
+
+
+    const command = client.commands.get(
+        interaction.commandName
+    );
+
+
+    if (!command) return;
+
+
+    await command.execute(interaction);
+
 });
+
 
 client.login(process.env.TOKEN);
