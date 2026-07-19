c20d542c7f5ed2c4770ca00532c41409c0682587
founderofmyself <lolitsthefamousjake@hotmail.com>
2026-07-18 20:40:05 -0400

    Update embeds.js

--- utils/embeds.js
+++ utils/embeds.js
@@ -1,1 +1,40 @@
+const { EmbedBuilder } = require("discord.js");
 
+function success(title, description) {
+    return new EmbedBuilder()
+        .setColor("#57F287")
+        .setTitle(`✅ ${title}`)
+        .setDescription(description)
+        .setTimestamp()
+        .setFooter({
+            text: "Fontein Systems"
+        });
+}
+
+function error(title, description) {
+    return new EmbedBuilder()
+        .setColor("#ED4245")
+        .setTitle(`❌ ${title}`)
+        .setDescription(description)
+        .setTimestamp()
+        .setFooter({
+            text: "Fontein Systems"
+        });
+}
+
+function info(title, description) {
+    return new EmbedBuilder()
+        .setColor("#5865F2")
+        .setTitle(`📢 ${title}`)
+        .setDescription(description)
+        .setTimestamp()
+        .setFooter({
+            text: "Fontein Systems"
+        });
+}
+
+module.exports = {
+    success,
+    error,
+    info
+};
